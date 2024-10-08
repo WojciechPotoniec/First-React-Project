9e670046f3663db5d999e0d5c8edbf4320bebc3b
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-10-08 10:09:05 +0200

    set CoreConcept function that return a component with props values

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-import imgPath from './assets/react-core-concepts.png'
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+import imgPath from "./assets/react-core-concepts.png";
+import { CORE_CONCEPTS } from "./data.js";
 
-function genRandomInt(max){
+function genRandomInt(max) {
   return Math.floor(Math.random() * (max + 1));
 }
 function Header() {
-const description = reactDescriptions[genRandomInt(2)];
+  const description = reactDescriptions[genRandomInt(2)];
 
   return (
     <header>
@@ -18,12 +19,30 @@ const description = reactDescriptions[genRandomInt(2)];
     </header>
   );
 }
+
+function CoreConcept({image, title, description}) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
 function App() {
   return (
     <div>
       <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <section id="core-concepts">
+          <h2>Time to get started!</h2>
+          <ul>
+            <CoreConcept {...CORE_CONCEPTS[0]}/>
+            <CoreConcept {...CORE_CONCEPTS[1]}/>
+            <CoreConcept {...CORE_CONCEPTS[2]}/>
+            <CoreConcept {...CORE_CONCEPTS[3]}/>
+          </ul>
+        </section>
       </main>
     </div>
   );

e7723aa4dc1a6a35265ba909473b9361856838b5
wojciechpotoniec <wojtekpotoniec@hotmail.it>
2024-10-17 12:52:20 +0200

    create new component to manage better useState and other functions

---
+++ src/components/Examples.jsx
@@ -0,0 +1,55 @@
+import { useState } from "react";
+import TabButton from "./TabButton/TabButton.jsx";
+import { EXAMPLES } from "../data.js";
+
+export default function Examples() {
+  const [selectedTopic, setSelectedTopic] = useState();
+  function handleSelected(selectedButton) {
+    setSelectedTopic(selectedButton);
+  }
+  let tabContent = <p>Please select a Topic.</p>;
+
+  if (selectedTopic) {
+    tabContent = (
+      <div id="tab-content">
+        <h3>{EXAMPLES[selectedTopic].title}</h3>
+        <p>{EXAMPLES[selectedTopic].description}</p>
+        <pre>
+          <code>{EXAMPLES[selectedTopic].code}</code>
+        </pre>
+      </div>
+    );
+  }
+  return (
+    <section id="examples">
+      <h2>Examples</h2>
+      <menu>
+        <TabButton
+          isSelected={selectedTopic === "components"}
+          onSelect={() => handleSelected("components")}
+        >
+          Components
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "jsx"}
+          onSelect={() => handleSelected("jsx")}
+        >
+          JSX
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "props"}
+          onSelect={() => handleSelected("props")}
+        >
+          Props
+        </TabButton>
+        <TabButton
+          isSelected={selectedTopic === "state"}
+          onSelect={() => handleSelected("state")}
+        >
+          State
+        </TabButton>
+      </menu>
+      {tabContent}
+    </section>
+  );
+}
